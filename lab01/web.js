fdc6bbde52d5897cb83696277fd60bea7e2dc872
bathanh321 <thanhnbgch211319@fpt.edu.vn>
2023-07-13 11:25:21 +0700

    port

--- lab01/web.js
+++ lab01/web.js
@@ -1,6 +1,13 @@
 //declare http library
 const http = require('http')
-//declare server
-const server = 'localhost'
+//declare host
+const host = 'localhost'
 //declare port of server
 const port = 3000 //3000:default port
+//declare server
+const server = http.createServer((request, respond) =>{
+    respond.write("<h1>Hello World!</h1>")
+    respond.end
+})
+//let server listen port to run web
+server.listen(port)
