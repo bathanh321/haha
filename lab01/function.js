462e44a8770de8b29d7d425ddfaa15a303b56ac6
bathanh321 <thanhnbgch211319@fpt.edu.vn>
2023-07-13 11:16:37 +0700

    push declare

--- lab01/function.js
+++ lab01/function.js
@@ -3,3 +3,14 @@ function sayHello(){
     console.log("Hello World")
 }
 sayHello()
+
+function greenwich(){
+    return "Greenwich Vietnam"
+}
+console.log("Hello " +greenwich())
+
+function caculateSum(a, b){
+    c = a + b
+    console.log("Total = "+c)
+}
+caculateSum(1, 2)
