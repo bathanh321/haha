6fa4411738889a84eec601e223ccfe73ce594a00
bathanh321 <thanhnbgch211319@fpt.edu.vn>
2023-07-13 11:39:53 +0700

    update localhost

--- lab01/web.js
+++ lab01/web.js
@@ -6,8 +6,11 @@ const host = 'localhost'
 const port = 3000 //3000:default port
 //declare server
 const server = http.createServer((request, respond) =>{
-    respond.write("<h1>Hello World!</h1>")
+    respond.write("<h1 style='color:Blue'>Hello World!</h1>")
+    respond.write("<h3 style='font-style: italic; background-color:yellow; text-decoration: underline;'>We are from Greenwich Vietnam</h3>")
     respond.end
 })
 //let server listen port to run web
-server.listen(port)
+server.listen(port, () =>{
+    console.log("Server is running at http://"+host+":"+port)
+})
